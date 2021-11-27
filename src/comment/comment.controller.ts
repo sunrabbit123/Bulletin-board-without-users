661e76088d25703cbc86378c7242b0f0c1ce2a1a
sunrabbit123 <qudwls185@naver.com>
2021-11-28 04:13:59 +0900

    테스트 필요

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,17 +1,35 @@
-import { Controller, Delete, Param, Patch, Post } from "@nestjs/common";
+import {
+  Controller,
+  Delete,
+  Param,
+  Patch,
+  Post,
+  Headers,
+  Body,
+} from "@nestjs/common";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { AuthMiddleware } from "src/util/auth";
+import { CommentService } from "./comment.service";
 
 @Controller("comment")
 @ApiTags("댓글 관련 API")
 export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
   @Post(":id")
   @ApiOperation({
     summary: "게시물 댓글 달기",
     description: "해당 게시물에 댓글을 달아드립니다~",
   })
   @AuthMiddleware.tokenValidate
-  async addCommentByPost(@Param("id") postId: number) {}
+  async addCommentByPost(
+    email: string,
+    @Headers("authorization") token: string,
+    @Param("id") postId: number,
+    @Body("content") content: string
+  ) {
+    this.commentService.addCommentByPost(postId, email, content);
+  }
 
   @Patch(":id")
   @ApiOperation({
@@ -19,7 +37,14 @@ export class CommentController {
     description: "id값에 해당하는 댓글을 수정합니다.",
   })
   @AuthMiddleware.tokenValidate
-  async patchCommentByPost(@Param("id") commentId: number) {}
+  async patchCommentByPost(
+    email: string,
+    @Headers("authorization") token: string,
+    @Param("id") commentId: number,
+    @Body("content") content: string
+  ) {
+    this.commentService.patchCommentByPost(commentId, email, content);
+  }
 
   @Delete(":id")
   @ApiOperation({
@@ -27,5 +52,11 @@ export class CommentController {
     description: "해당 id값에 해당하는 댓글을 삭제합니다.",
   })
   @AuthMiddleware.tokenValidate
-  async deleteCommentByPost(@Param("id") postId: number) {}
+  async deleteComment(
+    email: string,
+    @Headers("authorization") token: string,
+    @Param("id") commentId: number
+  ) {
+    this.commentService.deleteComment(commentId, email);
+  }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+import { Comment, Post, User } from "../entity";
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(
+    @InjectRepository(Comment) private commentRepository: Repository<Comment>,
+    @InjectRepository(Post) private postRepository: Repository<Post>,
+    @InjectRepository(User) private userRepository: Repository<User>
+  ) {}
+  public isNumeric(data: string | number): boolean {
+    return !isNaN(Number(data));
+  }
+
+  async addCommentByPost(id: number, email: string, content: string) {
+    if (!this.isNumeric(id)) {
+      throw new BadRequestException("id값이 숫자여야 합니다.");
+    }
+    const comment = new Comment();
+    comment.content = content;
+    comment.user = (await this.userRepository.find({ where: { email } }))[0];
+    comment.post = (await this.postRepository.find({ where: { id } }))[0];
+    return this.commentRepository.save(comment);
+  }
+
+  async patchCommentByPost(id: number, email: string, content: string) {
+    if (!this.isNumeric(id)) {
+      throw new BadRequestException("id값이 숫자여야 합니다.");
+    }
+    const comment = await this.commentRepository.findOne({ id });
+    if (comment.user.email != email) {
+      throw new ForbiddenException("권한이 없습니다");
+    }
+    comment.content = content ?? comment.content;
+
+    return this.commentRepository.save(comment);
+  }
+  async deleteComment(id: number, email: string) {
+    if (!this.isNumeric(id)) {
+      throw new BadRequestException("id값이 숫자여야 합니다.");
+    }
+
+    const targetComment = await this.commentRepository.findOne({
+      id,
+    });
+
+    if (!targetComment) {
+      throw new NotFoundException("찾을 수 없습니다.");
+    }
+
+    return this.commentRepository.remove(targetComment);
+  }
+}
